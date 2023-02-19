75cbd5e82891ada03b64406eeed8987854c1f46d
jenniferclarsson <jennifer.2.larsson@gmail.com>
2023-02-19 19:26:47 +0100

    fix: remove unneccessary comment relates to #7

--- linear_algebra/src/polynom_for_points.py
+++ linear_algebra/src/polynom_for_points.py
@@ -108,7 +108,6 @@ def points_to_polynomial(coordinates: list[list[int]]) -> str:
     while count < x:
         remove_e: list[str] = solution[count].split("e+" or "e-")
         if len(remove_e) > 1:
-            # Not tested
             solution[count] = f"{remove_e[0]}*10^{remove_e[1]}"
         solved += f"x^{x - (count + 1)}*{solution[count]}"
         if count + 1 != x:
